5e701ee5cd0b4dbdd51fcb3beef9106afffe5899
soffsossa-coder <soffsossa@gmail.com>
2026-06-29 00:05:27 -0300

    SresPR.m

--- SresPR.m
+++ SresPR.m
@@ -2,9 +2,10 @@ function Sres = SresPR(comp,T,P)
 
 out = PRcompuesto(comp,'TP',T,P);
 
-R = 8.314;
+R = 8.314462618;
 
-Z = out.Z;
+% Fase vapor: se selecciona la raiz de mayor Z (out.Z es vector cuando hay 3 raices)
+Z = out.Z(end);
 B = out.B;
 b = out.b;
 dadT = out.da_dT;
