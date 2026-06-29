e9fbade347887c6acfe5c1137022377d7aa98f03
soffsossa-coder <soffsossa@gmail.com>
2026-06-29 00:04:46 -0300

    HresPR.m

--- HresPR.m
+++ HresPR.m
@@ -2,9 +2,10 @@ function Hres = HresPR(comp,T,P)
 
 out = PRcompuesto(comp,'TP',T,P);
 
-R = 8.314;
+R = 8.314462618;
 
-Z = out.Z;
+% Fase vapor: se selecciona la raiz de mayor Z (out.Z es vector cuando hay 3 raices)
+Z = out.Z(end);
 a = out.a;
 b = out.b;
 B = out.B;
