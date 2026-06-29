ba25af50834a51bc440baae009cdc4d191285df9
soffsossa-coder <soffsossa@gmail.com>
2026-06-29 00:05:55 -0300

    UresPR.m

--- UresPR.m
+++ UresPR.m
@@ -2,10 +2,13 @@ function Ures = UresPR(comp,T,P)
 
 out = PRcompuesto(comp,'TP',T,P);
 
-R = 8.314;
+R = 8.314462618;
+
+% Fase vapor: se selecciona la raiz de mayor Z (out.Z es vector cuando hay 3 raices)
+Z = out.Z(end);
 
 Hres = HresPR(comp,T,P);
 
-Ures = Hres - R*T*(out.Z - 1);
+Ures = Hres - R*T*(Z - 1);
 
 end
